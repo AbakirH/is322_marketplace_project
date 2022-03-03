b82f246109531e496c423295ab62d21efb9d5002
abakirh <abakirhanna@gmail.com>
2022-03-02 19:38:55 -0500

    can sort items by price

--- pages/products.tsx
+++ pages/products.tsx
@@ -6,16 +6,13 @@ import { Footer } from "../components/Footer";
 import Card from "../components/Card";
 import { useEffect, useState } from "react";
 
-const ProductsPage: NextPage<{ products: Products[] }> = ({ products }) => {
+const ProductsPage: NextPage<{ products: Products[]; categories: any[] }> = ({
+  products,
+  categories,
+}) => {
   const [selectedCategories, setSelectedCategories] = useState([]) as any;
-
-  const categories: string[] = [];
-
-  products.forEach((product) => {
-    if (!categories.some((e) => e === product.category)) {
-      categories.push(product.category);
-    }
-  });
+  const [lowToHigh, setlowToHigh] = useState(false);
+  const [highToLow, sethighToLow] = useState(false);
 
   const getSelectedCategories = (category: string) => {
     if (selectedCategories.includes(category)) {
@@ -29,6 +26,7 @@ const ProductsPage: NextPage<{ products: Products[] }> = ({ products }) => {
 
   const filterProducts = () => {
     if (selectedCategories.length == 0) {
+      products = setPriceFilter();
       return products;
     } else {
       products = products.filter((product) => {
@@ -36,13 +34,34 @@ const ProductsPage: NextPage<{ products: Products[] }> = ({ products }) => {
           return product;
         }
       });
+      products = setPriceFilter();
       return products;
     }
   };
 
-  useEffect(() => {
-    console.log(selectedCategories);
-  }, [selectedCategories]);
+  const selectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    if (value == "lowToHigh") {
+      setlowToHigh(true);
+      sethighToLow(false);
+    } else if (value == "highToLow") {
+      sethighToLow(true);
+      setlowToHigh(false);
+    } else {
+      sethighToLow(false);
+      setlowToHigh(false);
+    }
+  };
+
+  const setPriceFilter = () => {
+    if (highToLow) {
+      return products.sort((a, b) => b.price - a.price);
+    } else if (lowToHigh) {
+      return products.sort((a, b) => a.price - b.price);
+    } else {
+      return products.sort((a, b) => a.title.localeCompare(b.title));
+    }
+  };
 
   return (
     <div id="home">
@@ -56,8 +75,7 @@ const ProductsPage: NextPage<{ products: Products[] }> = ({ products }) => {
               <div>
                 <input
                   type="checkbox"
-                  id="vehicle1"
-                  name="vehicle1"
+                  key={category}
                   value={category}
                   onChange={(e) => getSelectedCategories(category)}
                 />
@@ -66,16 +84,13 @@ const ProductsPage: NextPage<{ products: Products[] }> = ({ products }) => {
             );
           })}
           <h2>Filter by Price</h2>
-          <div>
-            <input type="checkbox" id="vehicle1" name="vehicle1" value="" />
-            <label>Low to High</label>
-          </div>
-          <div>
-            <input type="checkbox" id="vehicle1" name="vehicle1" value="" />
-            <label>High to Low</label>
-          </div>
+          <select onChange={selectChange} name="price" id="price">
+            <option value="recommended">Recommended</option>
+            <option value="lowToHigh">Low to High</option>
+            <option value="highToLow">High to Low</option>
+          </select>
         </div>
-        {console.log(selectedCategories.length)}
+        {console.log(products)}
         <div id="products">
           <Card products={filterProducts()} />
         </div>
@@ -87,11 +102,18 @@ const ProductsPage: NextPage<{ products: Products[] }> = ({ products }) => {
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const res = await fetch("https://fakestoreapi.com/products");
-  const products: Products = await res.json();
+  const products: Products[] = await res.json();
+  const categories: string[] = [];
 
+  products.forEach((product) => {
+    if (!categories.some((e) => e === product.category)) {
+      categories.push(product.category);
+    }
+  });
   return {
     props: {
       products: products,
+      categories: categories,
     },
   };
 };
